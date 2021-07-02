aa3ce16855149faa6c7c54961500107f07929651
Seb <seb.dargoeuves@gmail.com>
2021-07-02 09:44:30 +0100

    IPFClient - error handling

--- api_clients/ipf/ipf_api_client.py
+++ api_clients/ipf/ipf_api_client.py
@@ -1,6 +1,5 @@
 from typing import List, Dict, Optional
 import os
-
 from httpx import Client as httpxClient
 
 
@@ -148,7 +147,7 @@ class IPFClient(httpxClient):
         snapshot_id: Optional[str] = None,
     ):
         """
-        Method to fetch data from IP Fabric tables. D
+        Method to fetch data from IP Fabric tables.
 
         Takes parameters to select:
         * url - [mandatory] a string containing the API endpoint for the table to be queried
@@ -174,19 +173,31 @@ class IPFClient(httpxClient):
 
 
 class IPFDevice:
-    def __init__(self, name: str):
+    def __init__(self, hostname: str):
+        """
+        Initialise an IPFDevice object.
+
+        Sets properties:
+        * hostname = hostname of the device we are looking for in IP Fabric.
+        """
         ipf = IPFClient()
+
         device = ipf.fetch_table(
             "tables/inventory/devices",
             columns=["hostname", "siteName", "vendor", "platform", "loginIp"],
-            filters={"hostname": ["like", name]},
+            filters={"hostname": ["like", hostname]},
         )
-        self.hostname = device[0]["hostname"]
-        self.site = device[0]["siteName"]
-        self.vendor = device[0]["vendor"]
-        self.ipaddr = device[0]["loginIp"]
-        self.snmpv2 = self.getSNMP(ipf, self.hostname)
-        self.bestsnmpv2 = self.getBestSNMPComm(self.snmpv2)
+        if device != []:
+            self.hostname = device[0]["hostname"]
+            self.site = device[0]["siteName"]
+            self.vendor = device[0]["vendor"]
+            self.platform = device[0]["platform"]
+            self.ipaddr = device[0]["loginIp"]
+            self.snmpv2 = self.getSNMP(ipf, self.hostname)
+            self.bestsnmpv2 = self.getBestSNMPComm(self.snmpv2)
+            self.status = f"INFO - device '{self.hostname}' found"
+        else:
+            self.status = f"WARNING - device '{hostname}' not found"
         ipf.close()
 
     def getSNMP(self, ipf, name: str):
@@ -223,6 +234,9 @@ class IPFDevice:
             if replaceComm:
                 chosenComm["community"] = comm["name"]
                 chosenComm["auth"] = comm["authorization"]
-                chosenComm["acl"] = len(comm["acl"]) > 0
+                if comm["acl"]:
+                    chosenComm["acl"] = len(comm["acl"]) > 0
+                else:
+                    chosenComm["acl"] = False
 
         return chosenComm["community"]
