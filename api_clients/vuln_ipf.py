4bd1ea97a5b6be060df8dc2a2be4946894bb59d7
Justin Jeffery <justin.jeffery@ipfabric.io>
2021-11-18 10:25:12 -0500

    Refactor for cognitive complexity

--- api_clients/vuln_ipf.py
+++ api_clients/vuln_ipf.py
@@ -2,14 +2,35 @@ from ipf.ipf_api_client import IPFClient
 from nist_cve.nistcve_api_client import NistCVECheck
 
 
-def main():
+def fetch_cve(dev):
+    # if not, fetch up to the first 50 CVEs from NIST
+    print(
+        'CVEs not fetched before - requesting ' + str((dev['vendor'], dev['family'], dev['version'])) + ' ...')
+    try:
+        res = NistCVECheck(dev['vendor'], dev['family'], dev['version'])
+        return res.cves
+    except:
+        return ['Error']
+
+
+def print_cve(f, dev, cves):
+    f.write("'" + dev['hostname'] + "','" + dev['vendor'] + "','")
+    if dev['family']:
+        f.write(dev['family'] + "','")
+    else:
+        f.write("','")
+    f.write(dev['version'] + "','" + str({len(cves)}))
+    for c in cves:
+        f.write("','" + c)
+    f.write("\n")
+
+
+def main(screen=True, file='cve.csv'):
     print("Fetching IP Fabric inventory")
     ipf = IPFClient()
     devs = ipf.inventory.devices.all(columns=['hostname', 'vendor', 'family', 'version'],
                                      filters={"siteName": ["like", "L35"]})
     fetched_cves = dict()
-    screen = True
-    file = 'cve.csv'
 
     if file:
         f = open(file, "w")
@@ -18,17 +39,9 @@ def main():
     for dev in devs:
         combo = (dev['vendor'], dev['family'], dev['version'])
         if combo not in fetched_cves:
-            # if not, fetch up to the first 50 CVEs from NIST
-            print(
-                'CVEs not fetched before - requesting ' + str((dev['vendor'], dev['family'], dev['version'])) + ' ...')
-            try:
-                res = NistCVECheck(dev['vendor'], dev['family'], dev['version'])
-                fetched_cves.update({combo: res.cves})
-            except:
-                fetched_cves.update({combo: ['Error']})
+            fetched_cves[combo] = fetch_cve(dev)
 
         # output the result
-
         if screen:
             print(dev['hostname'], dev['vendor'], dev['family'], dev['version'])
             print(f'    No of CVEs: {len(fetched_cves[combo])}')
@@ -36,15 +49,7 @@ def main():
                 print('        ', c)
 
         if file:
-            f.write("'" + dev['hostname'] + "','" + dev['vendor'] + "','")
-            if dev['family']:
-                f.write(dev['family'] + "','")
-            else:
-                f.write("','")
-            f.write(dev['version'] + "','" + str({len(fetched_cves[combo])}))
-            for c in fetched_cves[combo]:
-                f.write("','" + c)
-            f.write("\n")
+            print_cve(f, dev, fetched_cves[combo])
 
     if file:
         f.close()
